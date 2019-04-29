61112a5c7112f5893333b462b9111e71ab58cd42
Matt Cappleman <matt.cappleman@gmail.com>
2019-04-28 22:31:30 -0400

    Fixing silly mistakes

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import pprint
 
 from pymongo import MongoClient
 
+DB_NAME = "drinen-mlb-feed"
+DB_HOST = "ds031203.mlab.com"
+DB_PORT = 31203
+DB_USER = "raspberrypi"
+DB_PASS = "Test1234"
+
 client = MongoClient(DB_HOST, DB_PORT)
 db = client[DB_NAME]
 db.authenticate(DB_USER, DB_PASS)
@@ -19,9 +25,11 @@ for game in games_collection.find({"date": {"$gt": start_date}}).sort("date"):
         continue
 
     duplicates += 1
+    print(gd2_id)
     print("Duplicates: " + str(duplicates))
+    # games_collection.delete_one({"gd2_id": gd2_id})
 
 
-# for key, value in found_gd2_ids.items():
-#    game = games_collection.find_one({"gd2_id": key})
-#    games_collection.delete_one({"_id": game["_id"]})
+#for key, value in found_gd2_ids.items():
+#   game = games_collection.find_one({"gd2_id": key})
+#   games_collection.delete_one({"_id": game["_id"]})
